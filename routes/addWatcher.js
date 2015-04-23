a971d2a2e8985426923a238ac332afbe98b8aaeb
Nicholas Hrynuik <nhrynuik@gmail.com>
2015-04-22 23:21:22 -0400

    Added price_min and price_max to db. Added related validation and update logic.

--- routes/addWatcher.js
+++ routes/addWatcher.js
@@ -19,14 +19,36 @@ router.post('/', function(req, res, next) {
   ];
   var formResult = {};
   fields.forEach(function (field) {
-    formResult[field] = req.body[field] ? req.body[field] : null;
+    formResult[field] = req.body[field] ? req.body[field].trim() : null;
   });
   if (formResult.email === null || formResult.city === null) {
     res.send('fail: missing email or city: ' + formResult);
   } else {
+    validateForm(formResult);
     database.addWatcher(formResult);
     res.send('success: ' + req.body.city + " ");
   }
 });
+
+function validateForm(formResult) {
+  formResult.priceMin = filterInt(formResult.priceMin);
+  if (formResult.priceMin === null) {
+    formResult.priceMin = 0;
+  }
+  formResult.priceMax = filterInt(formResult.priceMax);
+  if (formResult.priceMax === null) {
+    formResult.priceMax = 2147483646;
+  }
+  if (formResult.priceMax !== null && formResult.priceMin !== null) {
+    formResult.priceMax = Math.max(formResult.priceMin, formResult.priceMax);
+  }
+}
+
+function filterInt(value) {
+    if(/^(\+)?([0-9]+)$/.test(value)) {
+      return Number(value);
+    }
+    return null;
+}
 
 module.exports = router;
--- util/database.js
+++ util/database.js
@@ -1,16 +1,15 @@
 var pg = require('pg');
 var conString = "postgres://airbnbwatch_user:airbnbwatch_pass@localhost/airbnbwatch_dev";
 
-
 function addWatcher(formResult) {
   var client = new pg.Client(conString);
   client.connect (function(err) {
     if (err) {
       return console.error('could not connect to postgres', err);
     }
-    client.query('INSERT INTO watchers (email, city) ' +
-                 'VALUES ($1, $2)',
-        [formResult.email, formResult.city],
+    client.query('INSERT INTO watchers (email, city, price_min, price_max) ' +
+                 'VALUES ($1, $2, $3, $4)',
+        [formResult.email, formResult.city, formResult.priceMin, formResult.priceMax],
         function (err, result) {
           if (err) {
             return console.error('error running query', err);
