16ba60257bc6502f5dfbc600469c9d97f96f71f4
Nicholas Hrynuik <nhrynuik@gmail.com>
2015-04-23 20:58:30 -0400

    Added more validation and form processing.

--- routes/addWatcher.js
+++ routes/addWatcher.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var database = require('../util/database.js');
+var MAX_INT = 2147483646;
 
 /* Post new watcher. */
 router.post('/', function(req, res, next) {
@@ -14,7 +15,9 @@ router.post('/', function(req, res, next) {
     'numBedrooms',
     'numBathrooms',
     'numBeds',
-    'roomType',
+    'roomTypeEntire',
+    'roomTypePrivate',
+    'roomTypeShared',
     'email'
   ];
   var formResult = {};
@@ -24,31 +27,62 @@ router.post('/', function(req, res, next) {
   if (formResult.email === null || formResult.city === null) {
     res.send('fail: missing email or city: ' + formResult);
   } else {
-    validateForm(formResult);
-    database.addWatcher(formResult);
+    var valid = validateForm(formResult);
+    if (!valid) {
+      res.send('fail: form did not validate');
+      return;
+    }
+    console.log(formResult);
+    //database.addWatcher(formResult);
     res.send('success: ' + req.body.city + " ");
   }
 });
 
 function validateForm(formResult) {
-  formResult.priceMin = filterInt(formResult.priceMin);
-  if (formResult.priceMin === null) {
-    formResult.priceMin = 0;
-  }
-  formResult.priceMax = filterInt(formResult.priceMax);
-  if (formResult.priceMax === null) {
-    formResult.priceMax = 2147483646;
-  }
-  if (formResult.priceMax !== null && formResult.priceMin !== null) {
-    formResult.priceMax = Math.max(formResult.priceMin, formResult.priceMax);
-  }
+  formResult.priceMin = filterInt(formResult.priceMin, 0);
+  formResult.priceMin = clampInt(0, formResult.priceMin, MAX_INT);
+
+  formResult.priceMax = filterInt(formResult.priceMax, MAX_INT);
+  formResult.priceMax = clampInt(0, formResult.priceMax, MAX_INT);
+
+  if (formResult.email.length > 254 || formResult.email.length < 5) return false;
+  if (formResult.city.length > 1024 || formResult.email.length < 1) return false;
+
+  formResult.numGuests = filterInt(formResult.numGuests, 1);
+  formResult.numGuests = clampInt(1, formResult.numGuests, 16);
+
+  formResult.numBedrooms = filterInt(formResult.numBedrooms, 1);
+  formResult.numBedrooms = clampInt(1, formResult.numBedrooms, 10);
+
+  formResult.numBeds = filterInt(formResult.numBeds, 1);
+  formResult.numBeds = clampInt(1, formResult.numBeds, 16);
+
+  formResult.numBathrooms = filterHalfInt(formResult.numBathrooms, 1);
+  formResult.numBathrooms = clampInt(0, formResult.numBathrooms, 8);
+
+  formResult.roomTypeEntire = Boolean(formResult.roomTypeEntire);
+  formResult.roomTypePrivate = Boolean(formResult.roomTypePrivate);
+  formResult.roomTypeShared = Boolean(formResult.roomTypeShared);
+
+  return true;
 }
 
-function filterInt(value) {
-    if(/^(\+)?([0-9]+)$/.test(value)) {
+function filterInt(value, defaultVal) {
+    if(/^([0-9]+)$/.test(value)) {
       return Number(value);
     }
-    return null;
+    return defaultVal;
+}
+
+function filterHalfInt(value) {
+  if(/^([0-9]*)(\.5)?$/.test(value)) {
+    return Number(value)
+  }
+  return 0;
+}
+
+function clampInt(low, value, hi) {
+  return Math.max(low, Math.min(value, hi));
 }
 
 module.exports = router;
