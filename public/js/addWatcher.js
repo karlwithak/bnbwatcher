e644c04e557282c1d804744067a025e608a25a81
Nicholas Hrynuik <nhrynuik@gmail.com>
2015-04-23 22:48:59 -0400

    Implemented date picker.

--- public/js/addWatcher.js
+++ public/js/addWatcher.js
@@ -6,4 +6,22 @@ $(function(){
       console.log('response: ' + data)
     })
   });
+
+  var picker1 = new Pikaday({
+    field: $('input[name=moveIn]')[0],
+    firstDay: 1,
+    minDate: new Date('2000-01-01'),
+    maxDate: new Date('2020-12-31'),
+    yearRange: [2000,2020],
+    container: $("#moveInContainer")[0]
+  });
+
+  var picker2 = new Pikaday({
+    field: $('input[name=moveOut]')[0],
+    firstDay: 1,
+    minDate: new Date('2000-01-01'),
+    maxDate: new Date('2020-12-31'),
+    yearRange: [2000,2020],
+    container: $("#moveOutContainer")[0]
+  });
 });
