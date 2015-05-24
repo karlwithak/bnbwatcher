3a5c3ca8da8a249a72b992631f46f4fde3613147
Nicholas Hrynuik <nhrynuik@gmail.com>
2015-05-24 15:20:42 -0400

    Show user how many current rooms match the watcher. Everything, except extra message when rooms are too high or low, is finished. Addresses #23 Currently relying on nginx reverse proxying to get around CORS. This logic should also be implemented in nodeJs for comparability.

--- public/js/form.js
+++ public/js/form.js
@@ -1,33 +1,48 @@
 'use strict';
 
 $(function(){
+  var $fields = {
+    location     : $('input[name=location]'),
+    checkin      : $('input[name=checkin]'),
+    checkout     : $('input[name=checkout]'),
+    price_min    : $('input[name=price_min]'),
+    price_max    : $('input[name=price_max]'),
+    currency     : $('select[name=currency]'),
+    num_guests   : $('select[name=number_of_guests]'),
+    min_beds     : $('select[name=min_beds]'),
+    min_bedrooms : $('select[name=min_bedrooms]'),
+    min_baths    : $('select[name=min_bathrooms]'),
+    room_entire  : $('input[name=room_type_entire]'),
+    room_private : $('input[name=room_type_private]'),
+    room_shared  : $('input[name=room_type_shared]')
+  };
+  
+  
   // Calendar logic
   var milliseconsInDay = 1000 * 60 * 60 * 24;
   var millisecondsInYear = milliseconsInDay * 365;
   var now = new Date();
   var aYearFromNow = new Date(now.getTime() + millisecondsInYear);
 
-  var $checkin = $('input[name=checkin]');
-  var $checkout = $('input[name=checkout]');
   
   var checkin_picker = new Pikaday({
-    field: $checkin[0],
+    field: $fields.checkin[0],
     minDate: now,
     maxDate: aYearFromNow,
     container: $('#moveInContainer')[0]
   });
 
   var checkout_picker = new Pikaday({
-    field: $checkout[0],
+    field: $fields.checkout[0],
     minDate: now,
     maxDate: aYearFromNow,
     container: $('#moveOutContainer')[0]
   });
   
-  $checkin.change(function () {
-    var next_date = new Date($checkin.val());
+  $fields.checkin.change(function () {
+    var next_date = new Date($fields.checkin.val());
     if (isNaN(next_date.getTime())) {
-      $checkin.val('');
+      $fields.checkin.val('');
       checkout_picker.setMinDate(now);
       return;
     }
@@ -43,8 +58,8 @@ $(function(){
     updatePriceUnit();
   });
   
-  $checkout.change(function () {
-    var prev_date = new Date($checkout.val());
+  $fields.checkout.change(function () {
+    var prev_date = new Date($fields.checkout.val());
     if (isNaN(prev_date.getTime())) {
       checkin_picker.setMaxDate(aYearFromNow);
       return;
@@ -62,8 +77,8 @@ $(function(){
   });
 
   function updatePriceUnit() {
-    var checkin = new Date($checkin.val());
-    var checkout = new Date($checkout.val());
+    var checkin = new Date($fields.checkin.val());
+    var checkout = new Date($fields.checkout.val());
     if (isNaN(checkin.getTime()) || isNaN(checkout.getTime())) {
       return;
     }
@@ -77,45 +92,41 @@ $(function(){
 
 
   // Price logic
-  var $price_min = $('input[name=price_min]');
-  var $price_max = $('input[name=price_max]');
-
-  $price_max.change(function () {
-    if ($price_max.is(':valid')) {
-      $price_min.prop('max', $price_max.val());
+  $fields.price_max.change(function () {
+    if ($fields.price_max.is(':valid')) {
+      $fields.price_min.prop('max', $fields.price_max.val());
     }
   });
 
-  $price_min.change(function () {
-    if ($price_min.is(':valid')) {
-      $price_max.prop('min', $price_min.val());
+  $fields.price_min.change(function () {
+    if ($fields.price_min.is(':valid')) {
+      $fields.price_max.prop('min', $fields.price_min.val());
     }
   });
 
 
   // Location logic
   var locationService = new google.maps.places.AutocompleteService();
-  var $location_field = $('input[name=location]');
   var $prediction_list = $('ul#predictions');
   var $prediction_items = $prediction_list.find("li");
 
-  $location_field.on('input propertychange paste', handleLocationChange);
+  $fields.location.on('input propertychange paste', handleLocationChange);
 
   function handleLocationChange() {
-    if ($location_field.val().length === 0) {
+    if ($fields.location.val().length === 0) {
       $prediction_items.hide();
       return;
     } else {
       $prediction_items.show();
     }
     locationService.getPlacePredictions({
-          input: $location_field.val(),
+          input: $fields.location.val(),
           types: ['geocode']
         },
         updateLocationPredictions);
   }
 
-  function updateLocationPredictions(locations, status) {
+  function updateLocationPredictions(locations) {
     if (!locations) return;
     locations.forEach(function (location, index) {
       $prediction_items.eq(index).text(location.description);
@@ -123,6 +134,46 @@ $(function(){
   }
 
   $prediction_items.on('mousedown', function () {
-    $location_field.val(this.innerHTML);
+    $fields.location.val(this.innerHTML);
   });
+
+
+  // Number of current matches logic
+  var $currentRooms = $("#currentRooms");
+  var $form = $("form#signUp");
+
+  $form.on('change', checkCurrentRooms);
+
+  function checkCurrentRooms() {
+    if (!$fields.location.val()) {
+      $currentRooms.text(0);
+      return;
+    }
+    var url = "http://bnbwatcher.com/check-rooms/?" +
+        "https://m.airbnb.ca/api/-/v1/listings/search?items_per_page=1";
+    if ($fields.location.val()) url += "&location=" + $fields.location.val();
+    if ($fields.checkin.val()) url += "&checkin=" + checkin_picker.getDate().toJSON().substr(0, 10);
+    if ($fields.checkout.val()) url += "&checkout=" + checkout_picker.getDate().toJSON().substr(0, 10);
+    if ($fields.price_min.val()) url += "&price_min=" + $fields.price_min.val();
+    if ($fields.price_max.val()) url += "&price_max=" + $fields.price_max.val();
+    if ($fields.currency.val()) url += "&currency=" + $fields.currency.val();
+    if ($fields.num_guests.val()) url += "&guests=" + $fields.num_guests.val();
+    if ($fields.min_beds.val()) url += "&min_beds=" + $fields.min_beds.val();
+    if ($fields.min_bedrooms.val()) url += "&min_bedrooms=" + $fields.min_bedrooms.val();
+    if ($fields.min_baths.val()) url += "&min_bathrooms=" + $fields.min_baths.val();
+    if ($fields.room_entire.prop("checked")) url += "&room_types[]=Entire+home/apt";
+    if ($fields.room_private.prop("checked")) url += "&room_types[]=Private+room";
+    if ($fields.room_shared.prop("checked")) url += "&room_types[]=Shared+room";
+
+    $.get(url, updateCurrentRooms);
+  }
+
+  function updateCurrentRooms(data) {
+    if (!data) return;
+    var roomCount = data.listings_count;
+    if (roomCount >= 1000) {
+      roomCount = "1000+";
+    }
+    $currentRooms.text(roomCount);
+  }
 }());
